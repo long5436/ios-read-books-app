3399267282dc69c126a624b05640855dded0671e
Long <long@Longs-iMac-Pro.local>
2023-05-08 03:43:11 +0700

    update code firebase get data, create simple layout

---
+++ Ios Books/BookCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BookCollectionViewCell.swift
+//  Ios Books
+//
+//  Created by Long on 5/8/23.
+//  Copyright © 2023 Long. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    //MARK: Properties
+    @IBOutlet weak var bookTitleCell: UIView!
+    
+    //MARK: Contructor
+    init?(name: String) {
+        
+    }
+}
--- Ios Books/HomeController.swift
+++ Ios Books/HomeController.swift
@@ -11,26 +11,63 @@ import Firebase
 
 class HomeController: UIViewController {
     
+    var arrBook = [Book]()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //        getBooks()
+        
+    }
+    
+    // Lay du lieu cac cuon sach tu firebase
+    func getBooks(){
         let db = Firestore.firestore()
         let booksRef = db.collection("books")
-        
-        booksRef.getDocuments() { (querySnapshot, error) in
+        //
+        booksRef.order(by: "created", descending: true).limit(to: 10).getDocuments() { (querySnapshot, error) in
+            
+            
             if let error = error {
                 print("Error getting documents: \(error)")
             } else {
                 for document in querySnapshot!.documents {
-                    print("\(document.documentID) => \(document.data())")
+                    let data = document.data()
+                    
+                    guard
+                        let categoryId = data["cate_id"] as? String,
+                        let name = data["name"] as? String,
+                        let photo = data["photo"] as? String,
+                        let about = data["about"] as? String
+                        else {
+                            continue
+                    }
+                    
+                    //                    let storageRef = Storage.storage().reference(forURL: photo)
+                    //                    storageRef.downloadURL { (url, error) in
+                    //                        if let error = error {
+                    //                            print("Error downloading image URL: \(error.localizedDescription)")
+                    //                        } else {
+                    //                           let ok = url?.absoluteString ?? ""
+                    //                            print("OK \(ok)")
+                    //                        }
+                    //
+                    //                    }
+                    //
+                    let newBook = Book(bookId: document.documentID , categoryId: categoryId, name: name, photo: photo, about: about)
+                    if let newBook = newBook {
+                        
+                        self.arrBook.append(newBook)
+                    }
                 }
+                
+                print("Mang book \(self.arrBook.count)")
             }
         }
     }
-
-
+    
+    
 }
 
