f529e60acc86db72b34df8e1e32115f630182b47
Long <long@Longs-iMac-Pro.local>
2023-05-19 15:57:41 +0700

    create view history

---
+++ Ios Books/HistoryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HistoryViewController.swift
+//  Ios Books
+//
+//  Created by Long on 5/19/23.
+//  Copyright © 2023 Long. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    //MARK: Propperties
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookName: UILabel!
+    @IBOutlet weak var bookPage: UILabel!
+    @IBOutlet weak var btnReadBookContinue: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // bo cong nut
+        btnReadBookContinue.layer.cornerRadius = 8
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
